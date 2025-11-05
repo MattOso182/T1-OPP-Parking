7ece93e1e912b88358c79fa11c0888f2aac7bf6d
MattOso182 <mateo.oso182@gmail.com>
2025-11-04 20:24:22 -0500

    v0.6.1

--- 06Code/ParkingControlSystem/src/model/Resident.java
+++ 06Code/ParkingControlSystem/src/model/Resident.java
@@ -5,5 +5,58 @@ package model;
  * @author Team 1 - T.A.P. (The Art of Programming)
  */
 public class Resident {
-    
+
+    private int id;
+    private String name;
+    private String apartmentNumber;
+    private String vehiclePlate;
+    private boolean hasActivateRental;
+
+    public Resident(int id, String name, String apartmentNumber, String vehiclePlate, boolean hasActivateRental) {
+        this.id = id;
+        this.name = name;
+        this.apartmentNumber = apartmentNumber;
+        this.vehiclePlate = vehiclePlate;
+        this.hasActivateRental = hasActivateRental;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    public void setApartmentNumber(String apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    public String getVehiclePlate() {
+        return vehiclePlate;
+    }
+
+    public void setVehiclePlate(String vehiclePlate) {
+        this.vehiclePlate = vehiclePlate;
+    }
+
+    public boolean isHasActivateRental() {
+        return hasActivateRental;
+    }
+
+    public void setHasActivateRental(boolean hasActivateRental) {
+        this.hasActivateRental = hasActivateRental;
+    }
 }
---
+++ 06Code/ParkingControlSystem/src/view/ParkingControlSystemSimulator.java
@@ -0,0 +1,9 @@
+package view;
+
+/**
+ *
+ *  @author Team 1 - T.A.P. (The Art of Programming)
+ */
+public class ParkingControlSystemSimulator {
+    
+}
